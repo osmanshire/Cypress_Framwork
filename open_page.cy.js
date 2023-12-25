e3b5daa48a2d399e34a15017ac85bfd22a621366
osmanshire <44617622+osmanshire@users.noreply.github.com>
2023-12-25 05:41:23 +0000

    Add files via upload

--- open_page.cy.js
+++ open_page.cy.js
@@ -2,22 +2,20 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.telerik.com/support/demos')
 
-
     // accepting cookies
     .wait(2500)
 
-cy.get('#onetrust-accept-btn-handler').click
+cy.get('#onetrust-accept-btn-handler').click()
 
 //select dropdown
-.wait(2500)
 
-cy.get('.TK-Products-Menu-Item-Button').click
 .wait(2500)
+cy.get('.TK-Products-Menu-Item-Button').click()
+
 
 //select kendo ui
-
-
-cy.get('.TK-col-18 > .TK-row > :nth-child(1) > :nth-child(2) > [href="/kendo-ui"]').click
+.wait(2500)
+cy.get('.TK-col-18 > .TK-row > :nth-child(1) > :nth-child(2) > [href="/kendo-ui"]').click()
 
   })
 })
